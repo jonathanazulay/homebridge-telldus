2f572a257642fcc39f91183960391b439285cb4f
Jonathan Azulay <jonathanazulay@users.noreply.github.com>
2017-06-22 02:25:41 +0200

    wip

---
+++ login.js
@@ -0,0 +1,32 @@
+let wait = ms => new Promise((resolve) => setTimeout(resolve, ms))
+
+let retryPromise = (times, interval, func) => {
+  return func()
+  .catch(e => {
+    if (times === 0) {
+      throw e;
+    } else {
+      return wait(interval).then(() =>
+        retryPromise(times - 1, interval, func)
+      );
+    }
+  })
+};
+
+
+function login (session, appname, log) {
+  return session.initLogin(appname)
+    .then(initLogin => {
+      log(`Visit ${initLogin.authUrl} within 30 seconds and authorize the app. 1 year renewable token is recommended`)
+      return retryPromise(
+        30,
+        1000,
+        () => session.resumeLogin(initLogin.token)
+      )
+    })
+    .then(({ token }) => ({
+      token
+    }))
+}
+
+module.exports = login
